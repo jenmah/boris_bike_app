671951b1aa340326d0dd9b5401b15a0422dd070a
Jennifer Mah <jen.mah@hotmail.com>
2015-05-21 16:32:27 +0100

    refactored bike creation using let block

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,20 +2,20 @@ require_relative '../lib/bike'
 
 #We're describing the functionality of a specific class, Bike.
 describe Bike do
+
+  #The code below instantiates a new instance of the Bike class, and does the same thing as putting "bike = Bike.new" into each it/end thingy
+  let(:bike) { Bike.new }
   #This is a specific feature or behaviour that we EXPECT to have
   it 'should not be broken when created' do
-    bike = Bike.new #Instantiate a new instance of the Bike class
     expect(bike.broken?).to be false 
   end 
 
   it 'should be able to break' do
-    bike = Bike.new
     bike.break 
     expect(bike.broken?).to be true
   end
 
   it 'should be able to get fixed' do
-    bike = Bike.new
     bike.break
     bike.fix
     expect(bike.broken?). to be false
