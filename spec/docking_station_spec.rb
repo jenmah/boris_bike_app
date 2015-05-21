b9acaa34b143b1cd1a629dc495981da49c7dba7e
Jennifer Mah <jen.mah@hotmail.com>
2015-05-21 17:51:42 +0100

    passed the test for releasing a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../lib/docking_station'
 
 describe DockingStation do
-  
+
+  let(:bike) { Bike.new }
+  let(:station) { DockingStation.new }
+
   it 'should be able to accept a bike' do
     station = DockingStation.new
     bike = Bike.new
@@ -9,6 +12,12 @@ describe DockingStation do
     expect(station.bike_count).to eq 0
     station.dock(bike)
     expect(station.bike_count).to eq 1
+  end
+
+  it "should release a bike" do
+    station.dock(bike)
+    station.release(bike)
+    expect(station.bike_count). to eq 0
   end
 
 end
